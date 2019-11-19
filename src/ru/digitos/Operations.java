0ca3d7e520af9f890cb782891c79060ef40079a9
Artem Afonin <xfqdesnik@mail.ru>
2019-11-19 15:27:54 +0300

    Add operations & check operation.

---
+++ src/ru/digitos/Operations.java
@@ -0,0 +1,22 @@
+package ru.digitos;
+
+public class Operations {
+    int firstNumber = 0;
+    int secondNumber = 0;
+
+    int addition() {
+        return firstNumber + secondNumber;
+    }
+
+    int difference() {
+        return firstNumber - secondNumber;
+    }
+
+    int division() {
+        return firstNumber / secondNumber;
+    }
+
+    int multiplication() {
+        return firstNumber * secondNumber;
+    }
+}
